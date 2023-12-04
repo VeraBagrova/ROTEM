7d04506299db5690b5fa8f32589b66408871bb22
Too-Many-Bytes <fsigor11@gmail.com>
2023-12-04 18:31:40 +0300

    Formatting after merge

--- gui/main.py
+++ gui/main.py
@@ -101,106 +101,107 @@ class StartPage(tk.Frame):
 
 
 class EnterTheDataPage(tk.Frame):
-#     def check_number(self, entry_text: str):
-#         self.error_label = tk.Label()
-#         try:
-#             number = int(entry_text)
-#             if number < 2 or number > 14:
-#                 self.error_label.config(text="Число должно быть от 2 до 14", fg="red")
-#             else:
-#                 self.error_label.config(text="")
-#         except ValueError:
-#             self.error_label.config(text="Введите корректное число", fg="red")
-#
-#     def on_entry_change(self, *args):
-#         self.check_number(self.days_entry.get())
-#
-#     def __init__(self, parent, controller):
-#         tk.Frame.__init__(self, parent)
-#         label = ttk.Label(self, text="Enter the Data", font=LARGEFONT)
-#         label.grid(row=1, column=1, padx=10, pady=10, sticky='n')
-#
-#         label = ttk.Label(self, text="Enter amount of days under control", font=MIDDLEFONT)
-#         label.grid(row=2, column=1, padx=10, pady=10, sticky='n')
-#
-#         self.days_entry = tk.Entry(self, width=40, state='normal', validate="key",
-#                                    validatecommand=self.on_entry_change)
-#
-#         self.days_entry.grid(row=3, column=1, padx=10, pady=10)
+    #     def check_number(self, entry_text: str):
+    #         self.error_label = tk.Label()
+    #         try:
+    #             number = int(entry_text)
+    #             if number < 2 or number > 14:
+    #                 self.error_label.config(text="Число должно быть от 2 до 14", fg="red")
+    #             else:
+    #                 self.error_label.config(text="")
+    #         except ValueError:
+    #             self.error_label.config(text="Введите корректное число", fg="red")
+    #
+    #     def on_entry_change(self, *args):
+    #         self.check_number(self.days_entry.get())
+    #
+    #     def __init__(self, parent, controller):
+    #         tk.Frame.__init__(self, parent)
+    #         label = ttk.Label(self, text="Enter the Data", font=LARGEFONT)
+    #         label.grid(row=1, column=1, padx=10, pady=10, sticky='n')
+    #
+    #         label = ttk.Label(self, text="Enter amount of days under control", font=MIDDLEFONT)
+    #         label.grid(row=2, column=1, padx=10, pady=10, sticky='n')
+    #
+    #         self.days_entry = tk.Entry(self, width=40, state='normal', validate="key",
+    #                                    validatecommand=self.on_entry_change)
+    #
+    #         self.days_entry.grid(row=3, column=1, padx=10, pady=10)
 
-# amount_of_days = collect_entry_message(days_entry)
-# while amount_of_days:
-#     days_entry = tk.Entry(self, width=40, state='enabled')
+    # amount_of_days = collect_entry_message(days_entry)
+    # while amount_of_days:
+    #     days_entry = tk.Entry(self, width=40, state='enabled')
 
-        # for i in range(12):
-        #     for j in range(6):
-        #         # Create entry cell with some text inside
-        #         entry = tk.Entry(self, width=10)
-        #         entry.insert(tk.END, f"Row {i}, Column {j}")
-        #         entry.grid(row=4 + i, column=j, padx=10, pady=10)
+    # for i in range(12):
+    #     for j in range(6):
+    #         # Create entry cell with some text inside
+    #         entry = tk.Entry(self, width=10)
+    #         entry.insert(tk.END, f"Row {i}, Column {j}")
+    #         entry.grid(row=4 + i, column=j, padx=10, pady=10)
 
-        entry = tk.Entry(self, width=40, foreground='black', background='turquoise')
-        entry.insert(tk.END, f"Day (Sunday Red) ")
-        entry.grid(row=0, column=0, padx=2)
+    entry = tk.Entry(self, width=40, foreground='black', background='turquoise')
+    entry.insert(tk.END, f"Day (Sunday Red) ")
+    entry.grid(row=0, column=0, padx=2)
 
-        entry = tk.Entry(self, width=40, background='dark blue', foreground='white')
-        entry.insert(tk.END, "Storing mass at the end should be equal or more than ")
-        entry.grid(row=1, column=0, padx=2)
+    entry = tk.Entry(self, width=40, background='dark blue', foreground='white')
+    entry.insert(tk.END, "Storing mass at the end should be equal or more than ")
+    entry.grid(row=1, column=0, padx=2)
 
-        entry = tk.Entry(self, width=40, foreground='black', background='turquoise')
-        entry.insert(tk.END, "Ship 1 mass at the end should be equal to  ")
-        entry.grid(row=2, column=0, padx=2)
+    entry = tk.Entry(self, width=40, foreground='black', background='turquoise')
+    entry.insert(tk.END, "Ship 1 mass at the end should be equal to  ")
+    entry.grid(row=2, column=0, padx=2)
 
-        entry = tk.Entry(self, width=40, background='dark blue', foreground='white')
-        entry.insert(tk.END, "Ship 2 mass at the end should be equal to  ")
-        entry.grid(row=3, column=0, padx=2)
+    entry = tk.Entry(self, width=40, background='dark blue', foreground='white')
+    entry.insert(tk.END, "Ship 2 mass at the end should be equal to  ")
+    entry.grid(row=3, column=0, padx=2)
 
-        entry = tk.Entry(self, width=40, foreground='black', background='turquoise')
-        entry.insert(tk.END, "Day load to ship 1 left and right limits: ")
-        entry.grid(row=4, column=0, padx=2)
+    entry = tk.Entry(self, width=40, foreground='black', background='turquoise')
+    entry.insert(tk.END, "Day load to ship 1 left and right limits: ")
+    entry.grid(row=4, column=0, padx=2)
 
-        entry = tk.Entry(self, width=40, background='dark blue', foreground='white')
-        entry.insert(tk.END, "Day load to ship 2 left and right limits: ")
-        entry.grid(row=5, column=0, padx=2)
+    entry = tk.Entry(self, width=40, background='dark blue', foreground='white')
+    entry.insert(tk.END, "Day load to ship 2 left and right limits: ")
+    entry.grid(row=5, column=0, padx=2)
 
-        entry = tk.Entry(self, width=40, foreground='black', background='turquoise')
-        entry.insert(tk.END, "Penalty for ship1 unloading ")
-        entry.grid(row=6, column=0, padx=2)
+    entry = tk.Entry(self, width=40, foreground='black', background='turquoise')
+    entry.insert(tk.END, "Penalty for ship1 unloading ")
+    entry.grid(row=6, column=0, padx=2)
 
-        for i in range(7):
-            for j in range(6):
-                # Create entry cell with some text inside
-                if i % 2 == 0:
-                    entry = tk.Entry(self, width=15, foreground='black', background='turquoise')
-                else:
-                    entry = tk.Entry(self, width=15, foreground='white', background='dark blue')
+    for i in range(7):
+        for j in range(6):
+            # Create entry cell with some text inside
+            if i % 2 == 0:
+                entry = tk.Entry(self, width=15, foreground='black', background='turquoise')
+            else:
+                entry = tk.Entry(self, width=15, foreground='white', background='dark blue')
 
-                entry.insert(tk.END, f" ")
-                entry.grid(row=i, column=1 + j, padx=2)
+            entry.insert(tk.END, f" ")
+            entry.grid(row=i, column=1 + j, padx=2)
 
-        next_button = ttk.Button(
-            self,
-            text="Back",
-            command=lambda: controller.show_frame(Page2)
-        )
-        opt_button = ttk.Button(
-            self,
-            text="Optimal solution",
-            command=lambda: controller.show_frame(Page2)
-        )
-        next_button.grid(row=7, column=1, columnspan=2, pady=12)
-        opt_button.grid(row=7, column=4, columnspan=2, pady=12)
-        # intro_label = ttk.Label(self, text=intro, font=SMALLFONT, width=100)
-        # intro_label.grid(row=10, column=1, padx=10, pady=10, sticky="w")
-        #
-        # # button to show frame 2 with text
-        # # layout2
-        # button1 = ttk.Button(self, text="Enter the data",
-        #                      command=lambda: controller.show_frame(StartPage))
-        #
-        # # putting the button in its place
-        # # by using grid
-        # button1.grid(row=12, column=1, padx=10, pady=10)
+    next_button = ttk.Button(
+        self,
+        text="Back",
+        command=lambda: controller.show_frame(Page2)
+    )
+    opt_button = ttk.Button(
+        self,
+        text="Optimal solution",
+        command=lambda: controller.show_frame(Page2)
+    )
+    next_button.grid(row=7, column=1, columnspan=2, pady=12)
+    opt_button.grid(row=7, column=4, columnspan=2, pady=12)
+    # intro_label = ttk.Label(self, text=intro, font=SMALLFONT, width=100)
+    # intro_label.grid(row=10, column=1, padx=10, pady=10, sticky="w")
+    #
+    # # button to show frame 2 with text
+    # # layout2
+    # button1 = ttk.Button(self, text="Enter the data",
+    #                      command=lambda: controller.show_frame(StartPage))
+    #
+    # # putting the button in its place
+    # # by using grid
+    # button1.grid(row=12, column=1, padx=10, pady=10)
+
 
 # button to show frame 2 with text
 # layout2
